4787c84c2a4774cc4388ebad8539e7434d3d7eea
brunoclaumari <brunoclaumari@gmail.com>
2019-07-28 21:47:53 -0300

    Acrescentado tracos

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -6,10 +6,12 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("--------------------------------");
             Console.WriteLine("Olá Mundo!");            
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
             Console.WriteLine("Tchau!");
+            Console.WriteLine("--------------------------------");
 
             Console.ReadLine();
         }
